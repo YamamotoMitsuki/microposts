7c6e2a4cb56b673f245ec5f60ec4155f5d5ed409
mitsuki.yamamoto <2191205@shodai.ac.jp>
2019-06-14 11:25:48 +0000

    favorites

--- resources/views/micropost_favorite/favorite_button.blade.php
+++ resources/views/micropost_favorite/favorite_button.blade.php
@@ -1,11 +1,11 @@
 @if
-    @if (Auth::user()is_favoriting($micropost->id))
-        {!! Form::open(['route' => ['user.unFavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-block"]) !!}
+    @if (Auth::user()->is_favoriting($micropost->id))
+        {!! Form::open(['route' => ['micropost.unfavorite', $micropost->id], 'method' => 'DELETE']) !!}
+        {!! Form::submit('Unfavorite', ['class' => 'btn btn-danger btn-sm']) !!}
         {!! Form::close() !!}
     @else
-        {!! Form::open(['route' => ['user.favorite', $micropost->id]]) !!}
-            {!! Form::submit('favorite', ['class' => "btn btn-primary btn-block"]) !!}
+        {!! Form::open(['route' => ['micropost.favorite', $micropost->id], 'method' => 'POST']) !!}
+        {!! Form::submit('favorite', ['class' => 'btn btn-danger btn-sm' ]) !!}
         {!! Form::close() !!}
     @endif
 @endif
